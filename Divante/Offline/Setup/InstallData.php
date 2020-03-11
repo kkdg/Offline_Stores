4702e2f7291c5ff96417cfe8d7ec9e225449f17d
Degi <jonghapyounger@hanmail.net>
2020-03-11 14:04:56 +0900

    Add Data installer

--- Divante/Offline/Setup/InstallData.php
+++ Divante/Offline/Setup/InstallData.php
@@ -5,9 +5,22 @@ namespace Divante\Offline\Setup;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Divante\Offline\Model\StoresFactory;
 
 class InstallData implements InstallDataInterface
 {
+    /**
+     * @var StoresFactory
+     */
+    protected $storesFactory;
+
+    public function __construct(
+        StoresFactory $stores
+    )
+    {
+        $this->storesFactory = $stores;
+    }
+
     /**
      * Function install
      * @param ModuleDataSetupInterface $setup
@@ -16,6 +29,30 @@ class InstallData implements InstallDataInterface
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        //install data here
+        $data = [
+            [
+                'name' => 'Warszawa',
+                'address' => 'Koci pw. w. Michaa Archanioa',
+                'phone' => '+48 22 845 46 04',
+                'is_active' => 1
+            ],
+            [
+                'name' => 'Wroclaw',
+                'address' => 'Ulica Romana Dmowskiego 17, 50-203 Wrocaw, Poland',
+                'phone' => '+48 577 032 500',
+                'is_active' => 1
+            ],
+            [
+                'name' => 'Seoul',
+                'address' => 'Seoul GangNam Street 1st 12',
+                'phone' => '+82 10 2364 3330',
+                'is_active' => 1
+            ]
+        ];
+
+        foreach( $data as $datum ) {
+            $stores = $this->storesFactory->create();
+            $stores->setData($datum)->save();
+        }
     }
 }
